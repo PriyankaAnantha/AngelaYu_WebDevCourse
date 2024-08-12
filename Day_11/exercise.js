dce3a04520e23fcb7dd96fb50c595112971e4fc0
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-12 12:21:07 +0530

    feat: Add function to randomly select a buyer for lunch

--- Day_11/exercise.js
+++ Day_11/exercise.js
@@ -1,1 +1,8 @@
 var names= ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+function who_buys_lunch(names){
+    var buyerpicker = Math.random();
+    buyerpicker = Math.floor( (buyerpicker* names.length) +1 ) ; 
+    var finalbuyer = names[buyerpicker];
+    console.log(finalbuyer);
+    return finalbuyer;  
+}
