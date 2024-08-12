fd8c2e01a24e4af3792d7eeee5926d23bf19417b
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-12 15:20:12 +0530

    Refactor beer function to use a loop and fix formatting

--- Day_11/exercise2.js
+++ Day_11/exercise2.js
@@ -15,3 +15,18 @@ function beer() {
     }
     beer(); 
     
+
+
+    // Angela's solution 
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
