c8679b1f07c6876ff9093eecd025ae39488fa970
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-13 19:56:14 +0530

    feat: Refactor drum kit code to handle both click and keydown events

--- Day_12/002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
+++ Day_12/002 Drum-Kit-Starting-Files/Drum Kit Starting Files/index.js
@@ -3,8 +3,20 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
        var buttonInnerHTML = this.innerHTML;
+         makeSound(buttonInnerHTML);
 
-    switch (buttonInnerHTML) {
+    
+}
+
+);
+}
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -33,12 +45,8 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
             var kick = new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
-        default: console.log(buttonInnerHTML);
+        default: console.log(key);
 
     }
+
 }
-
-);
-}
-
-
