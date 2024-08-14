d944d98c5c6d11872bcce57a156c5bb485ac536f
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-14 11:07:45 +0530

    feat: Refactor playSound function in Simon Game Challenge

--- Day_13/003 Simon-Game-Challenge-Starting-Files/Simon Game Challenge Starting Files/index.js
+++ Day_13/003 Simon-Game-Challenge-Starting-Files/Simon Game Challenge Starting Files/index.js
@@ -6,6 +6,13 @@ var gamePattern = [];
 gamePattern.push(randomChosenColour);
 
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);   // this will flash the button of the randomChosenColour
+playSound(randomChosenColour);
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
 
 function nextSequence(){
     var randomNumber = (Math.floor((Math.random() * 4)+0));
