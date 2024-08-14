832fdc4b791e4782255fc9f704ba064d6aa64ed0
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-14 12:46:11 +0530

    Refactor game logic and improve user experience in Simon Game Challenge

--- Day_13/003 Simon-Game-Challenge-Starting-Files/Simon Game Challenge Starting Files/index.js
+++ Day_13/003 Simon-Game-Challenge-Starting-Files/Simon Game Challenge Starting Files/index.js
@@ -9,48 +9,50 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-// Event listener for key press to start the game
-$(document).keypress(function() {
+// Function to start or restart the game
+function startGame() {
     if (!started) {
-        // Update level title and start the game
-        $("#level-title").text("Level " + level);
-        nextSequence();
         started = true;
+        nextSequence();
+    } else {
+        startOver();
+        nextSequence();
     }
+}
+
+// Event listener for key press to start or restart the game
+$(document).keypress(function() {
+    startGame();
 });
 
-// Event listener for button click
+// Event listener for button click to start or restart the game
+$("#start-button").click(function() {
+    startGame();
+});
+
+// Event listener for color button clicks
 $(".btn").click(function() {
-    // Get the color of the clicked button
     var userChosenColour = $(this).attr("id");
-    // Add the color to the user's clicked pattern
     userClickedPattern.push(userChosenColour);
 
-    // Play sound and animate button press
     playSound(userChosenColour);
     animatePress(userChosenColour);
 
-    // Check if the user's pattern is correct
-    checkAnswer(userClickedPattern.length-1);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 // Function to check if the user's pattern is correct
 function checkAnswer(currentLevel) {
-    // Check if the current color in the game pattern matches the user's clicked pattern
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        // If the user has completed the pattern, proceed to the next level
-        if (userClickedPattern.length === gamePattern.length){
-            setTimeout(function () {
-                nextSequence();
-            }, 1000);
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(nextSequence, 1000);
         }
     } else {
-        // If the user's pattern is incorrect, play wrong sound, show game over message, and restart the game
         playSound("wrong");
         $("body").addClass("game-over");
-        $("#level-title").text("Game Over, Press Any Key or start button to Restart");
+        $("#level-title").text("Game Over, Press Any Key or Start Button to Restart");
 
-        setTimeout(function () {
+        setTimeout(function() {
             $("body").removeClass("game-over");
         }, 200);
 
@@ -60,28 +62,22 @@ function checkAnswer(currentLevel) {
 
 // Function to generate the next color in the game pattern
 function nextSequence() {
-    // Reset the user's clicked pattern
     userClickedPattern = [];
-    // Increment the level and update level title
     level++;
     $("#level-title").text("Level " + level);
-    // Generate a random number between 0 and 3
+
     var randomNumber = Math.floor(Math.random() * 4);
-    // Get the color corresponding to the random number
     var randomChosenColour = buttonColours[randomNumber];
-    // Add the color to the game pattern
     gamePattern.push(randomChosenColour);
 
-    // Show the color by fading in and out
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    // Play sound for the color
     playSound(randomChosenColour);
 }
 
 // Function to animate button press
 function animatePress(currentColor) {
     $("#" + currentColor).addClass("pressed");
-    setTimeout(function () {
+    setTimeout(function() {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
@@ -99,7 +95,24 @@ function startOver() {
     started = false;
 }
 
+// Modal functionality
+var modal = document.getElementById("game-description-modal");
+var btn = document.getElementById("description-btn");
+var span = document.getElementsByClassName("close-btn")[0];
 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 // logic of the game
@@ -121,56 +134,3 @@ function startOver() {
 // 16. The game will restart the game when the player presses any key after the game is over.
 
 
-
-
-// Get the modal
-var modal = document.getElementById("game-description-modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("description-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-btn")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-$(document).ready(function() {
-    var gameStarted = false;
-  
-    // Function to start the game
-    function startGame() {
-      if (!gameStarted) {
-        gameStarted = true;
-        // Your existing game start logic here
-        nextSequence();
-      } else {
-        startOver();
-        nextSequence();
-      }
-    }
-  
-    // Start game on keypress
-    $(document).keypress(function() {
-      startGame();
-    });
-  
-    // Start game on button click
-    $("#start-button").click(function() {
-      startGame();
-    });
-  });
