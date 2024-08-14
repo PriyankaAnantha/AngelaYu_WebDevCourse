81445a04dfc6c592bd3fe15eb66fe5a084e5a7f8
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-14 11:00:05 +0530

    feat: Generate random number for next sequence in Simon Game

---
+++ Day_13/003 Simon-Game-Challenge-Starting-Files/Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,17 @@
+
+function nextSequence(){
+    var randomNumber = (Math.floor((Math.random() * 4)+0));
+    // the above line will generate a random number between 0 and 2. 
+    // Math.random() generates a random number between 0 and 1.
+    // Math.floor() will round off the number to the nearest integer.
+    // So, if Math.random() generates 0.5, then Math.floor(0.5) will be 0.
+
+    //*****************************************************
+    // if i want a radom number of range x to y, then i will use Math.floor(Math.random() * (y - x + 1)) + x) 
+    //*****************************************************
+    //why +1? because Math.random() generates a number between 0 and 1, so if i want to generate a number between 0 and 2, then i will use Math.floor(Math.random() * (2 - 0 + 1)) + 0 = Math.floor(Math.random() * 3) + 0 = Math.floor(Math.random() * 3) = 0, 1, 2. 
+    // why +x? because i want to start the range from x.
+
+    console.log(randomNumber);
+}
+nextSequence();
