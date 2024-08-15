0ac83664d51baefe13af26c4cefe3563c87c890d
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-15 10:36:50 +0530

    Update hello.txt with corrected capitalization

--- Day_14/005 2.2-Native-Modules/2.2 Native Modules/index.js
+++ Day_14/005 2.2-Native-Modules/2.2 Native Modules/index.js
@@ -3,9 +3,15 @@ const fs =  require('fs'); // Importing the fs module to read the file system in
 const hello = 'Hello from Node.js';
 const newhello = 'New content';
 
-fs.writeFile('hello.txt', hello, (err) => { // Writing the content to the file
-    if (err) throw err;
-    console.log('File is created successfully.');
-});
+// fs.writeFile('hello.txt', hello, (err) => { // Writing the content to the file
+//     if (err) throw err;
+//     console.log('File is created successfully.');
+// });
 
+// read and write can be done at the same time? 
+
+fs.readFile('hello.txt', 'utf8', (err, data) => { // Reading the content from the file  
+    if (err) throw err;
+    console.log(data);
+});
 
