acf38b58554deb096f43bcc94c6a830b91d92f06
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-18 18:15:48 +0530

    refactor: Improve EJS rendering in index.js

--- Day_17/002 4.1-EJS-Tags/4.1 EJS Tags/index.js
+++ Day_17/002 4.1-EJS-Tags/4.1 EJS Tags/index.js
@@ -2,14 +2,15 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => 
+  {
   const data = {
     title: "EJS Tags",
     seconds: new Date().getSeconds(),
     items: ["apple", "banana", "cherry"],
     htmlContent: "<strong>This is some strong text</strong>",
   };
-  
+
   res.render("index.ejs", data);
 });
 
