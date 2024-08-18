0c9ee726972b3d38be98494a6a1e6300b2a8ceeb
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-18 17:11:50 +0530

    chore: Update npm dependencies and remove unused files

---
+++ Day_17/001 4.0-EJS/4.0 EJS/index.js
@@ -0,0 +1,16 @@
+import express from express;
+import path from path;
+import ejs from ejs;
+
+const app = express();
+const port = 3000;
+
+// use js getday method to build a website that gives advice based on the day of the week
+
+
+app.get ("/", (req, res) => {
+    res.render("index.ejs", {daytype: });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
