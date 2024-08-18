c8edeffe80ce831213a94fcb1bc72816c9ad3b9c
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-18 19:03:31 +0530

    refactor: Clear form after data submission

--- Day_17/003 4.2-Passing-Data/4.2 Passing Data/index.js
+++ Day_17/003 4.2-Passing-Data/4.2 Passing Data/index.js
@@ -8,14 +8,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
-  
 });
 
 app.post("/submit", (req, res) => {
-  const numLetters = req.body["fName"].length + req.body["lName"].length; 
-  res.render("index.ejs", { numLetters: numLetters });
+  let numLetters= 0; 
+  numLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { numberOfLetters: numLetters });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+// even after refreshing the page, the form seems to still hold onto the data that was previously entered
+// this is because the form is still holding onto the data that was previously entered
+// how can we clear the form after the data has been submitted?
+// we can clear the form by adding a redirect to the get route
+// app.post("/submit", (req, res) => {
+//   let numLetters= 0;
+//   numLetters = req.body["fName"].length + req.body["lName"].length;
+//   res.render("index.ejs", { numberOfLetters: numLetters });
+// });
