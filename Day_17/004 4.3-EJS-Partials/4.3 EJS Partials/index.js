e718a7a2a18df5b1edf30d05593c5c4481a5ecb5
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-08-18 20:30:38 +0530

    refactor: Update npm dependencies and remove unused files

--- Day_17/004 4.3-EJS-Partials/4.3 EJS Partials/index.js
+++ Day_17/004 4.3-EJS-Partials/4.3 EJS Partials/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
@@ -31,6 +34,37 @@ app.get("/contact", (req, res) => {
 );
 
 
+app.post("/contact", (req, res) => {
+  
+  let submitted = false; 
+  let name = req.body["name"];
+  let email = req.body["email"];
+  let text = req.body["text"];
+
+  res.render("contact.ejs", { name: name, email: email, text: text, submitted: true });
+  // after the form is submitted, the page needs to be re rendered to show thank you message. how to do that ?
+ // what logic to add in the ejs file to show the thank you message after the form is submitted ?
+ // answer is to add an if condition in the ejs file to check if the form is submitted or not.
+ // here is the code to add in the ejs file
+  // <% if (submitted) { %>
+  // <h2>Thank you for your message!</h2>
+  // <% } %>
+  // now the ejs file will show the thank you message after the form is submitted.
+  // what is this submitted variable ? how to set it to true after the form is submitted ?
+  // answer is to pass the submitted variable to the ejs file when rendering it.
+  // here is the code to add in the index.js file
+  // res.render("contact.ejs", { name: name, email: email, text: text, submitted: true });
+  // how to set the submitted variable to true after the form is submitted ?// how should i declare the submitted variable in the js file ?
+  // answer is to declare the submitted variable in the js file and set it to true after the form is submitted.
+  // here is the code to add in the index.js file
+  // let submitted= false;
+  // let name = req.body["name"];
+  // let email = req.body["email"];
+  // let text = req.body["text"];
+  // res.render("contact.ejs", { name: name, email: email, text: text, submitted: true });
+  // now the submitted variable is set to true after the form is submitted.
+} 
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
